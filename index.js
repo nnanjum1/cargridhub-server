6dae9ca0278b87e26363b57bb5143c79f5d77e41
Najmun Nahar Anjum <najmunnanjum121@gmail.com>
2026-05-19 02:37:58 +0600

    set up mongodb connection

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require('dotenv')
-const app = express();
 dotenv.config()
 
+const uri = process.env.MONGODB_URI;
+const app = express();
 const PORT = process.env.PORT;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     console.log("server is ok")
