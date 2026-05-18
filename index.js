9da24d552ef1bd9465951d38577d64c9fbde916b
Najmun Nahar Anjum <najmunnanjum121@gmail.com>
2026-05-19 02:19:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv')
+const app = express();
+dotenv.config()
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    console.log("server is ok")
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
