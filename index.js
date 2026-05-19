52d9fe2a4bfb2f3101d3b3a55aaab3de07364597
Najmun Nahar Anjum <najmunnanjum121@gmail.com>
2026-05-19 23:30:27 +0600

    a new database cargridhub  and new collection cars made

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require('express');
+const cors = require('cors')
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require('dotenv')
 dotenv.config()
@@ -9,6 +11,9 @@ dotenv.config()
 const uri = process.env.MONGODB_URI;
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -21,10 +26,21 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
+        const db = client.db("cargridhub")
+        const carCollection = db.collection("cars")
+
+        app.post("/cars", async (req, res) => {
+            const carData = req.body;
+            const result = await carCollection.insertOne(carData)
+
+            res.json(result)
+        })
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
